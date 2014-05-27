92f607a332651064df30217e34166d040151fd05
Candy & Camille <candybibby@users.noreply.github.com>
2014-05-27 10:58:46 +0200

    Squelette de la classe PanneauChoix

--- src/PanneauChoix.java
+++ src/PanneauChoix.java
@@ -1,8 +1,70 @@
+import java.awt.image.ImageObserver;
+import java.awt.MenuContainer;
+import java.io.Serializable;
+import javax.accessibility.Accessible;
+import javax.swing.*;
+import java.awt.*;
+/**
+ * Classe PanneauChoix
+ * @author
+ * Classe pour la construction de l'interface utilisateur
+ * Hérite de JPanel
+ */
+public class PanneauChoix extends JPanel{
 
-public class PanneauChoix {
+		//ATTRIBUTS
+	
+	/** Dessin*/
+	private DessinFigures dessin;
+	
+	/** Figure à construire*/
+	private FigureColoree fc;
+	
 
-	public PanneauChoix() {
-		// TODO Auto-generated constructor stub
+
+		//CONSTRUCTEUR
+	
+	/**
+	 * Constructeur de la classe
+	 * @param DessinFigures
+	 */
+	public PanneauChoix(DessinFigures d){
+		this.dessin = d;
 	}
-
+	
+	
+		//METHODES
+	
+	/**
+	 * Méthode implémentant la création
+	 * d'une figure géométrique
+	 * @param int
+	 * @return FigureColoree
+	 */
+	private FigureColoree creeFigure(int index){
+		
+		//modifier ça
+		return this.fc;
+	}
+	
+	/**
+	 * Méthode déterminant la couleur à utiliser
+	 * @param int
+	 * @return Color
+	 */
+	private Color determineCouleur(int index){
+		
+		
+		//modifier ça
+		return Color.BLACK;
+	}
+	
+	/**
+	 * Redéfinition de la méthode paintComponent
+	 * @param g
+	 */
+	public void paintComponent(Graphics g){
+		
+	}
+	
 }
