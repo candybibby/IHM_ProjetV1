975a0ee4cc13b36561e9d6ac60ef852ded9cdde1
Candy & Camille <candybibby@users.noreply.github.com>
2014-05-27 11:26:46 +0200

    javadoc Fenetre

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,8 +1,38 @@
+import javax.swing.*;
 
-public class Fenetre {
+/**
+ * Cette classe définit l'interface utilisateur et la méthode main.
+ * @author Candy & Camille
+ *
+ */
+public class Fenetre extends JFrame {
 
-	public Fenetre() {
-		// TODO Auto-generated constructor stub
+	
+	
+			// ATTRIBUTS
+	
+	/** Contenu de la fenêtre principale (JPanel) */
+	private DessinFigures dessin;
+	
+	
+			// CONSTRUCTEUR
+	
+	/**
+	 * Constructer de la classe
+	 * permettant de construire une fenetre avec une taille et un titre définit
+	 * @param s - titre de la fenetre
+	 * @param w - largeur de la fenetre
+	 * @param h - hauteur de la fenetre
+	 */
+	public Fenetre(String s, int w, int h) {
+		
+	}
+	
+	
+			// METHODE PRINCIPALE
+	
+	public static void main (String [] args){
+		
 	}
 
 }
